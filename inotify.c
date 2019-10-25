2045d690cc69b4e3e92bc6cef3b4d1ef5d640e8e
AntonioAvalos <201114339@ingenieria.usac.edu.gt>
2019-10-25 17:23:53 -0600

    Update inotify.c

--- inotify.c
+++ inotify.c
@@ -1,6 +1,5 @@
-#include  <stdio.h>
-#include  <signal.h>
-#include  <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <errno.h>
 #include <sys/types.h>
 #include <sys/inotify.h>
@@ -9,67 +8,65 @@
 #define EVENT_SIZE  (sizeof(struct inotify_event))
 #define BUF_LEN     (1024 * (EVENT_SIZE + 16))
 
-void  INThandler(int);
-FILE* fptr;
-
-int  main(void)
-{	
-    signal(SIGINT, INThandler);
-	fptr = fopen("./log.txt", "a");
-	int length, i = 0;
+int main(int argc, char **argv) {
+	
+   
+    int length, i = 0;
     int fd;
     int wd;
     char buffer[BUF_LEN];
-	fd = inotify_init();
+
+	FILE* fptr;
+	
+
+    fd = inotify_init();
+
     if (fd < 0) {
         perror("inotify_init");
-    }	
-    wd = inotify_add_watch(fd, ".",IN_MODIFY | IN_CREATE | IN_DELETE);	
-    while (1){
-		i = 0;	  
+    }
+
+    wd = inotify_add_watch(fd, ".",
+        IN_MODIFY | IN_CREATE | IN_DELETE);
+	
+	
+	while(1)
+    {
+		//fptr = fopen("./log.txt", "a");
+		i = 0;
+	  
 		length = read(fd, buffer, BUF_LEN);
+
 		if (length < 0) {
 			perror("read");
 		}
+
 		while (i < length) {
 			struct inotify_event *event = (struct inotify_event *) &buffer[i];
 			if (event->len) {
 				if (event->mask & IN_CREATE) {
-					printf("The file %s was created.\n", event->name);					
-					fprintf(fptr,"The file %s was created.\n",event->name);
+					printf("The file %s was created.\n", event->name);
+					
+					//fprintf(fptr,"The file %s was created.\n",event->name);
 				} else if (event->mask & IN_DELETE) {
-					printf("The file %s was deleted.\n", event->name);					
-					fprintf(fptr,"The file %s was deleted.\n", event->name);
+					printf("The file %s was deleted.\n", event->name);
+					
+					//fprintf(fptr,"The file %s was deleted.\n", event->name);
 				} else if (event->mask & IN_MODIFY) {
-					printf("The file %s was modified.\n", event->name);					
-					fprintf(fptr,"The file %s was modified.\n", event->name);
+					printf("The file %s was modified.\n", event->name);
+					
+					//fprintf(fptr,"The file %s was modified.\n", event->name);
 				}
 			}
 			i += EVENT_SIZE + event->len;
 		}
-        pause();		
+		//fclose(fptr);
 	}
-	fclose(fptr);
-	(void) inotify_rm_watch(fd, wd);
+	
+	
+	
+	
+    (void) inotify_rm_watch(fd, wd);
     (void) close(fd);
-	 printf ("Exiting inotify example...\n");
-     return 0;
-}
-
-void  INThandler(int sig)
-{
-     char  c;
-
-     signal(sig, SIG_IGN);
-     printf("OUCH, did you hit Ctrl-C?\n"
-            "Do you really want to quit? [y/n] ");
-     c = getchar();
-     if (c == 'y' || c == 'Y'){
-		  fclose(fptr);
-		  exit(0);
-	 }
-     else{
-		signal(SIGINT, INThandler);
-	 }
-     getchar(); // Get new line character
+	printf ("Exiting inotify example...\n");
+	return 1;
 }
