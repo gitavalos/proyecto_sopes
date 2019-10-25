63b8aeb03b9d53f0ac2c1705b5bdf1855fd5d22b
AntonioAvalos <201114339@ingenieria.usac.edu.gt>
2019-10-25 17:22:07 -0600

    Update inotify.c

--- inotify.c
+++ inotify.c
@@ -23,11 +23,9 @@ int  main(void)
 	fd = inotify_init();
     if (fd < 0) {
         perror("inotify_init");
-    }
-	
-    wd = inotify_add_watch(fd, ".",IN_MODIFY | IN_CREATE | IN_DELETE);
-	
-     while (1){
+    }	
+    wd = inotify_add_watch(fd, ".",IN_MODIFY | IN_CREATE | IN_DELETE);	
+    while (1){
 		i = 0;	  
 		length = read(fd, buffer, BUF_LEN);
 		if (length < 0) {
@@ -37,23 +35,20 @@ int  main(void)
 			struct inotify_event *event = (struct inotify_event *) &buffer[i];
 			if (event->len) {
 				if (event->mask & IN_CREATE) {
-					printf("The file %s was created.\n", event->name);
-					
+					printf("The file %s was created.\n", event->name);					
 					fprintf(fptr,"The file %s was created.\n",event->name);
 				} else if (event->mask & IN_DELETE) {
-					printf("The file %s was deleted.\n", event->name);
-					
+					printf("The file %s was deleted.\n", event->name);					
 					fprintf(fptr,"The file %s was deleted.\n", event->name);
 				} else if (event->mask & IN_MODIFY) {
-					printf("The file %s was modified.\n", event->name);
-					
+					printf("The file %s was modified.\n", event->name);					
 					fprintf(fptr,"The file %s was modified.\n", event->name);
 				}
 			}
 			i += EVENT_SIZE + event->len;
 		}
-        //pause();
-	 }
+        pause();		
+	}
 	fclose(fptr);
 	(void) inotify_rm_watch(fd, wd);
     (void) close(fd);
