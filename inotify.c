37e119659fdf31989583e25ec6f4e62974c17c51
AntonioAvalos <201114339@ingenieria.usac.edu.gt>
2019-10-25 16:20:25 -0600

    Update inotify.c

--- inotify.c
+++ inotify.c
@@ -1,72 +1,28 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <errno.h>
-#include <sys/types.h>
-#include <sys/inotify.h>
-#include <unistd.h>
+#include  <stdio.h>
+#include  <signal.h>
+#include  <stdlib.h>
 
-#define EVENT_SIZE  (sizeof(struct inotify_event))
-#define BUF_LEN     (1024 * (EVENT_SIZE + 16))
+void     INThandler(int);
 
-int main(int argc, char **argv) {
-	
-   
-    int length, i = 0;
-    int fd;
-    int wd;
-    char buffer[BUF_LEN];
+int  main(void)
+{
+     signal(SIGINT, INThandler);
+     while (1)
+          pause();
+     return 0;
+}
 
-	FILE* fptr;
-	
+void  INThandler(int sig)
+{
+     char  c;
 
-    fd = inotify_init();
-
-    if (fd < 0) {
-        perror("inotify_init");
-    }
-
-    wd = inotify_add_watch(fd, ".",
-        IN_MODIFY | IN_CREATE | IN_DELETE);
-	
-	
-	while(1)
-    {
-		fptr = fopen("./log.txt", "a");
-		i = 0;
-	  
-		length = read(fd, buffer, BUF_LEN);
-
-		if (length < 0) {
-			perror("read");
-		}
-
-		while (i < length) {
-			struct inotify_event *event = (struct inotify_event *) &buffer[i];
-			if (event->len) {
-				if (event->mask & IN_CREATE) {
-					printf("The file %s was created.\n", event->name);
-					
-					fprintf(fptr,"The file %s was created.\n",event->name);
-				} else if (event->mask & IN_DELETE) {
-					printf("The file %s was deleted.\n", event->name);
-					
-					fprintf(fptr,"The file %s was deleted.\n", event->name);
-				} else if (event->mask & IN_MODIFY) {
-					printf("The file %s was modified.\n", event->name);
-					
-					fprintf(fptr,"The file %s was modified.\n", event->name);
-				}
-			}
-			i += EVENT_SIZE + event->len;
-		}
-		fclose(fptr);
-	}
-	
-	
-	
-	
-    (void) inotify_rm_watch(fd, wd);
-    (void) close(fd);
-
-    return 0;
+     signal(sig, SIG_IGN);
+     printf("OUCH, did you hit Ctrl-C?\n"
+            "Do you really want to quit? [y/n] ");
+     c = getchar();
+     if (c == 'y' || c == 'Y')
+          exit(0);
+     else
+          signal(SIGINT, INThandler);
+     getchar(); // Get new line character
 }
