9423986f2e3209ba637267a81e13ac0ee58302eb
AntonioAvalos <201114339@ingenieria.usac.edu.gt>
2019-11-19 16:46:07 -0600

    t

--- modulo/lkm_example.c
+++ modulo/lkm_example.c
@@ -75,11 +75,12 @@ static int __init init_my_module(void)
 	//para este ejemplo se utiliza la llamada al sistema openat para abir archivos
 	printk(KERN_INFO "Inside kernel space\n");
 	//cambiando permisos de la pagina
-	//make_rw((unsigned long)sys_call_table);
+	make_rw((unsigned long)sys_call_table);
 	//guardando el valor de memoria de la llamada original
 	original_sys_unlink = (void *)xchg(&sys_call_table[__NR_unlink], hacked_sys_unlink);
 	//insertando nuestra funcion a la direccion de memoria de openat
 	//*(sys_call_table + __NR_unlink) = (unsigned long) hacked_sys_unlink;
+	make_ro((unsigned long)sys_call_table);
 	printk("hizo el cambio de pagina \n");
 	return 0;
 }
@@ -88,12 +89,12 @@ static void __exit cleanup_my_module(void)
 {
 	
 	//cambiando la direccion de memoria a modo de escritura
-	//make_rw((unsigned long)sys_call_table);
+	make_rw((unsigned long)sys_call_table);
 	//regresando la funcion original a la direccion de la llamada
 	//*(sys_call_table + __NR_unlink) = (unsigned long)original_sys_unlink;
 	//cambiando la direccion de memoria a modo de lectura. 
 	xchg(&sys_call_table[__NR_unlink], original_sys_unlink);
-	//make_ro((unsigned long)sys_call_table);
+	make_ro((unsigned long)sys_call_table);
 	printk(KERN_INFO "Exiting kernel space\n");
 	return;
 }
