bb337585551d00afb28f4cd16f7c23bf392b2425
AntonioAvalos <201114339@ingenieria.usac.edu.gt>
2019-11-19 12:03:53 -0600

    test

--- inotify.c
+++ inotify.c
@@ -8,6 +8,53 @@
 #define EVENT_SIZE  (sizeof(struct inotify_event))
 #define BUF_LEN     (1024 * (EVENT_SIZE + 16))
 
+void add_watches(int fd, char *root)
+{
+  int wd;
+  char *abs_dir;
+  struct dirent *entry;
+  DIR *dp;
+ 
+  dp = opendir(root);
+  if (dp == NULL)
+    {
+      perror("Error opening the starting directory");
+      exit(0);
+    }
+ 
+  /* add watch to starting directory */
+  wd = inotify_add_watch(fd, root, IN_CREATE | IN_MODIFY | IN_DELETE); 
+  if (wd == -1)
+    {
+      fprintf(fp_log,"Couldn't add watch to %s\n",root);
+    }
+  else
+    {
+      printf("Watching:: %s\n",root);
+    }
+ 
+  /* Add watches to the Level 1 sub-dirs*/
+  abs_dir = (char *)malloc(MAX_LEN);
+  while((entry = readdir(dp)))
+    { 
+      /* if its a directory, add a watch*/
+      if (entry->d_type == DT_DIR)
+        {
+          strcpy(abs_dir,root);
+          strcat(abs_dir,entry->d_name);
+           
+          wd = inotify_add_watch(fd, abs_dir, IN_CREATE | IN_MODIFY | IN_DELETE); 
+          if (wd == -1)
+              printf("Couldn't add watch to the directory %s\n",abs_dir);
+          else
+            printf("Watching:: %s\n",abs_dir);
+        }
+    }
+   
+  closedir(dp);
+  free(abs_dir);
+}
+
 int main(int argc, char **argv) {   
     int length, i = 0;
     int fd;
@@ -18,7 +65,8 @@ int main(int argc, char **argv) {
     if (fd < 0) {
         perror("inotify_init");
     }
-    wd = inotify_add_watch(fd, "/home",IN_MODIFY | IN_CREATE | IN_DELETE);	
+    //wd = inotify_add_watch(fd, "/home",IN_MODIFY | IN_CREATE | IN_DELETE);	
+	add_watches(fd,"/home");
 	//fptr = fopen("/log.txt", "w+");	
 		//fprintf(fptr,"HELLO WORLD");
 		//fclose(fptr);
